234b7f8870d6dff48504f7b88767edcf9534f0ee
ehrhartesponda582 <ehrhartesponda582@gmail.com>
2025-03-22 17:15:12 +0000

    Add 18.go

---
+++ src/18.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    name := "GitHub School Project"
+    age := 18
+    color := "red"
+    shape := "circle"
+
+    fmt.Printf("Hello, %s! You are %d years old. Your favorite color is %s and your favorite shape is a %s.\n", name, age, color, shape)
+}
