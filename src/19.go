c7ce50a3097d42b7d0ffe1048fb05dd3afc6bd8f
ehrhartesponda582 <ehrhartesponda582@gmail.com>
2025-03-23 20:06:52 +0000

    Add 19.go

---
+++ src/19.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Define variables
+    var a, b float64 = 2.0, 3.0
+    sum := a + b
+
+    // Output result
+    fmt.Println("The sum of", a, "and", b, "is:", sum)
+}
