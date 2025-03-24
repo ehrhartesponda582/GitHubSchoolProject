48b20c7bf5743dd8bc5e811c9e58e76a3363eac5
ehrhartesponda582 <ehrhartesponda582@gmail.com>
2025-03-24 08:55:52 +0000

    Add 20.go

---
+++ src/20.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x int = 5
+    if x == 0 {
+        fmt.Println("x is zero")
+    } else if x > 0 {
+        fmt.Println("x is positive")
+    }
+}
